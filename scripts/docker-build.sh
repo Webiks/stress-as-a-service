2897fe2aa02f766f7c2384a9b1af9d586cd5f827
paz <chaim@webiks.com>
2019-11-06 14:54:16 +0200

    build & tag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 docker build . -t webiks/cron_server:"$PACKAGE_VERSION"
+docker tag webiks/cron_server:"$PACKAGE_VERSION" webiks/cron_server:latest
